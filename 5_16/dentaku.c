2bc8eca4392299964bf3a98cc2243cb45122df78
popon000 <hyp-880-zrw@e-net.nara.jp>
2025-05-30 06:14:31 +0000

    5_30

--- 5_16/dentaku.c
+++ 5_16/dentaku.c
@@ -6,49 +6,40 @@ float kakeru(float a,float b);
 int main (void){
 int mode;
 float a,b,n;
-printf("モード選択(1:+,2:-,3:*)\n");
-scanf("%f",&mode);
+
+printf("モード選択を選択してください。1.足し算2.引き算3.かけ算\n");
+scanf("%d",&mode);
+
 printf("１つ目の値の入力:\n");
 scanf("%f",&a);
-printf("２つ目の値の入力");
+
+printf("２つ目の値の入力\n");
 scanf("%f",&b);
 
-switch(mode){
-    case 1;
-    printf("足し算を選択しました。\n");
-    n=tasu(a,b);
-    break;
-    case 2;
-    printf("引き算を選択しました。\n");
-    n=hiku(a,b);
-    break;
-    case 3
-    printf("掛け算を選択しました。\n");
-    n=kakeru(a,b);
-    break;
-    defult
-    
+if(mode==1){
+printf("足し算を選択しました\n");
+n=tasu(a,b);
+}else if(mode==2){
+printf("引き算を選択しました\n");
+n=hiku(a,b);
+}else if(mode==3){
+printf("かけ算を選択しました\n");
+n=kakeru(a,b);    
+}else{
+printf("不正なモードです\n");    
 }
-
-
-
+printf("答えは%dです。\n",n);
 return 0;
 }
 
 float tasu(float a, float b){
-    float n;
-    n=a+b;
-    return n;
+    return a+b;
 }
 float hiku(float a, float b){
-    float n;
-    n=a-b;
-    return n;
+    return a-b;
 }
 float kakeru(float a, float b){
-    float n;
-    n=a*b;
-    return n;
+    return a*b;
 }
 
 
